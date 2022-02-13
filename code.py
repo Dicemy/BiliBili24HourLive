3c8d963ce313ad462d3c5e9a310aac5afb24ff8e
Dicemy <1583925267@qq.com>
2022-02-13 19:59:45 +0800

    添加自定义帧率

--- code.py
+++ code.py
@@ -4,6 +4,7 @@ import io
 
 dir = input("请输入视频所在文件夹地址（或者文件地址）:\n")
 rtmp = input("请输入B站直播串流密钥:\n")
+fps = input("请输入要进行推流的帧率:\n")
 
 nameList = []
 
@@ -37,5 +38,5 @@ def main():
             fw.write(file)
             fw.write('\n')
             filename = getfilename(file)
-            os.system('ffmpeg -re -i "' + file + '" -vcodec libx264 -acodec copy -b:a 192k -r 13 -vf "drawtext=fontsize=24:fontfile=FreeSerif.ttf:text=\'' + filename + '\':x=10:y=main_h-30:fontcolor=LightGrey:alpha=0.6" -f flv "rtmp://live-push.bilivideo.com/live-bvc/' + rtmp + '"')
+            os.system('ffmpeg -re -i "' + file + '" -vcodec libx264 -acodec copy -b:a 192k -r ' + fps + ' -vf "drawtext=fontsize=24:fontfile=FreeSerif.ttf:text=\'' + filename + '\':x=10:y=main_h-30:fontcolor=LightGrey:alpha=0.6" -f flv "rtmp://live-push.bilivideo.com/live-bvc/' + rtmp + '"')
 main()
